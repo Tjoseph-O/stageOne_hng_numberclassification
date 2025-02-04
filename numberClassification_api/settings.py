c54f454f45779b21e3faeb697e9dd7c1037bf54e
Tjoseph-O <mrtemitopejoseph@gmail.com>
2025-02-04 14:17:09 +0100

    update

--- numberClassification_api/settings.py
+++ numberClassification_api/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['temitopejosephstageonehng.up.railway.app']
 
 
 # Application definition
